02f0150cb77e5dd80b6b19c373c296e06aa73848
Ceclik <yura11122345@gmail.com>
2024-04-14 17:11:34 +0300

    Enemy kill effect added

--- Assets/Scripts/PlayerScripts/EnemiesKiller.cs
+++ Assets/Scripts/PlayerScripts/EnemiesKiller.cs
@@ -7,12 +7,16 @@ namespace PlayerScripts
     {
         [SerializeField] private float enemyDeathDelay;
 
+        [Space(10)] [SerializeField] private ParticleSystem enemyKillEffect;
+
         private GameObject _enemy;
         private void OnTriggerEnter2D(Collider2D other)
         {
             if (other.transform.parent.gameObject.CompareTag("Enemy"))
             {
                 _enemy = other.transform.parent.gameObject;
+                enemyKillEffect.transform.position = _enemy.transform.position;
+                enemyKillEffect.Play();
                 StartCoroutine(EnemyDeath());
             }
         }
